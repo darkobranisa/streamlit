bbddd4846c13e43ee4954171afda486f714319c3
darkobranisa <97313061+darkobranisa@users.noreply.github.com>
2022-09-30 20:23:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
 import streamlit
+
 streamlit.title('My Parents New healthy Dinner')
+
+streamlit.header('Breakfest Menu')
+
+streamlit.text('Hard boiled eggs')
+
+streamlit.text('Oatmeal')
+
+streamlit.text('Spinach, Kale')
