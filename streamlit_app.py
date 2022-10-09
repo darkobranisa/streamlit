eb6ae82884a2a328aaea1013c24edf9c7ab1319e
darkobranisa <97313061+darkobranisa@users.noreply.github.com>
2022-10-09 21:30:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,10 +14,8 @@ streamlit.text('🥑🍞 Avocado Toast')
   
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
@@ -25,7 +23,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #Display the table
 streamlit.dataframe(fruits_to_show)
-
 
 streamlit.header("Fruityvice Fruit Advice!")
 
@@ -38,12 +35,10 @@ try:
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         streamlit.dataframe(fruityvice_normalized)
     
-    except URLError as e:
-        streamlit.error()
-
+except URLError as e:
+    streamlit.error()
+        
 streamlit.stop()
-
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
